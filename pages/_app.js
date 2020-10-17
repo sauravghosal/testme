80ac434c84beba273a748b71dbc16dc21c10fcc6
sauravghosal <sghosal9@gatech.edu>
2020-10-17 13:50:11 -0400

    user login caching in localstorage

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,7 @@
 import "../styles/globals.css";
-import App, { Container } from "next/app";
-import Page from "../components/Page";
 
-class MyApp extends App {
-  render() {
-    const { Component } = this.props;
-    return (
-      <Container>
-        <Page></Page>
-      </Container>
-    );
-  }
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
