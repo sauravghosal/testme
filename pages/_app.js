5e145fbc4968a22904e25b273d3f695cea4762a0
sauravghosal <sghosal9@gatech.edu>
2020-10-17 11:21:03 -0400

    code refactoring

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import App, { Container } from "next/app";
+import Page from "../components/Page";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+class MyApp extends App {
+  render() {
+    const { Component } = this.props;
+    return (
+      <Container>
+        <Page></Page>
+      </Container>
+    );
+  }
 }
 
-export default MyApp
+export default MyApp;
