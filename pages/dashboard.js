3a417e1674bd9eca29c87a4a99f0787dbb798c2f
Rylie Geohegan <rgeohegan3@gatech.edu>
2020-10-17 14:14:29 -0400

    Dashboard initial setup
    dashboard initial setup

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -2,7 +2,6 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
-import Head from 'next/head'
 
 export default function Dashboard() {
     return (
@@ -11,7 +10,44 @@ export default function Dashboard() {
             <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
             </link>
             </Head>
-            <div className = {styles.Header}>
+            <h1>Dashboard</h1>
+            <div className = {styles.Dashboard}>
+            <div className="col">
+            <div className="card">
+                <a>Most Recent Test</a>
+            </div>
+            <br></br>
+            <br></br>
+                <div className="card m-3">
+                    <div className="card entire">
+                        <div className="card-body p-0 ">
+                            <div className="card-header border-secondary">
+                                <h5>Week 10/5/2020</h5>
+                            </div>
+                            <div className="card-text bg-status">
+                                <ul >
+                                    <li>Status: Pending</li>
+                                    <li>Updated: 10/7/2020</li>
+                                </ul>
+                                <br></br>
+                            </div>
+                        </div>
+                    </div>
+                    <center><buttons id={styles.mybtn}>Show barcode</buttons></center>
+                                {/* <img alt="barcode"></img>  */}
+                </div>
+                    <center> <buttons id={styles.mybtn}><Link href="/newtest">
+                    <a>+ New Test</a>
+                        </Link></buttons></center>
+            </div> 
+            </div> 
+            </div>
+    )
+}
+
+
+
+            {/* <div className = {styles.Header}>
                 <div className = {styles.Title}>
                     <h1>Dashboard</h1>
                 </div>
@@ -32,9 +68,6 @@ export default function Dashboard() {
                         <li>Updated: 10/7/2020</li>
                     </ul>
                     <buttons id={styles.mybtn}>Show barcode</buttons>
-                    {/* <img alt="barcode"></img> */}
-                </div>
-        </div>
-    </div>
-    )
-}
+                    <img alt="barcode"></img> 
+        //         {/* </div>
+        // </div> */}
