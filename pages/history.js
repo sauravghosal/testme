941ceda0a724707ce50037f7f752801dd489d32a
sauravghosal <sghosal9@gatech.edu>
2020-10-17 22:53:47 -0400

    file downloading

--- pages/history.js
+++ pages/history.js
@@ -1,9 +1,11 @@
 import Card from "../components/Card";
 import Button from "react-bootstrap/Button";
+import dynamic from "next/dynamic";
 
 const testingHistory = [
   { date: new Date("10/1/2020"), result: 0 },
   { date: new Date("10/5/2020"), result: 1 },
+  { date: new Date("3/23/1999"), result: 0 },
   { date: new Date("10/8/2020"), result: 0 },
   { date: new Date("10/10/2020"), result: 1 },
   { date: new Date("10/17/2020"), result: 2 },
@@ -41,19 +43,41 @@ const testingHistoryGroupedByDate = testingHistory.reduce((acc, test) => {
   return acc;
 }, {});
 
-export default function History() {
+function History() {
+  let html2pdf;
+
+  React.useEffect(() => {
+    html2pdf = require("html2pdf.js");
+  }, []);
+
+  const handleClick = () => {
+    const element = document.getElementById("testing-history");
+    var opt = {
+      margin: 1,
+      filename: "testing-history.pdf",
+      image: { type: "jpeg", quality: 0.98 },
+      html2canvas: { scale: 2 },
+      jsPDF: { unit: "in", format: "letter", orientation: "landscape" },
+    };
+    // New Promise-based usage:
+    html2pdf().from(element).set(opt).save();
+  };
+
   return (
-    <div>
+    <div id="testing-history">
       <h1>Testing History</h1>
       <div className="col">
         {Object.entries(testingHistoryGroupedByDate).map((val) => {
-          console.log(val);
           return <Card testWeek={val[0]} tests={val[1]} />;
         })}
       </div>
-      <Button variant="primary">
+      <Button variant="primary" onClick={handleClick}>
         <i className="fa fa-download"></i> Save Testing History
       </Button>
     </div>
   );
 }
+
+export default dynamic(() => Promise.resolve(History), {
+  ssr: false,
+});
